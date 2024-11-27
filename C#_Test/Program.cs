3b19f4f2825d87054d9e4157aa0bc98471fd8f4b
AdnanAlB <h24adalb@du.se>
2024-11-27 09:18:02 +0100

    Initial commit

---
+++ C#_Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är ditt namn?");
+
+           string  userinput = Console.ReadLine();
+            Console.WriteLine("Hej " + userinput + "!");
+
+        }
+    }
+}
